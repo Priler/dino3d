6d93bf393152481d83cffdc42799e7aecc58f2f4
Priler <priler96@gmail.com>
2020-02-15 17:48:23 +0500

    Update 2 rebuild

--- js/build.js
+++ js/build.js
@@ -2472,7 +2472,7 @@ load_manager.set_loader('misc', ['ground'], function() {
                    'rocks/0', 'rocks/1', 'rocks/2', 'rocks/3', 'rocks/4',
                    'flowers/0', 'flowers/1', 'flowers/2',
                    'trees/dead', 'trees/green',
-                   'fish/0', 'fish/1', 'fish/2', 'seaweed']; // 'PalmTree'
+                   'fish/0', 'fish/1', 'fish/2', 'seaweed'];
 
   for(let i = 0; i < miscItems.length; i++) {
     // load all flowers
