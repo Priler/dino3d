93cd9eee355ebf4d9f6235380aea510c2e82d80c
thePeras <jtopereira@gmail.com>
2024-02-07 18:06:31 +0000

    crouching

--- js/src/body_movements_manager.js
+++ js/src/body_movements_manager.js
@@ -1,8 +1,15 @@
+const Pose = {
+  NORMAL: 'NORMAL',
+  JUMPING: 'JUMPING',
+  CROUCHING: 'CROUCHING'
+};
+
 class BodyMovementsManager {
   constructor() {
     this.model = poseDetection.SupportedModels.MoveNet;
     this.detector = null;
     this.webcam = null;
+    this.state = Pose.NORMAL;
     this.init();
   }
 
@@ -30,6 +37,29 @@ class BodyMovementsManager {
     return keypoints_names.every(name => poseKeypointNames.includes(name));
   }
 
+  getKeypoint(pose, keypointName) {
+    if(pose.keypoints){
+      return pose.keypoints.find(keypoint => keypoint.name === keypointName);
+    }
+  }
+
+  isCrouching(pose){
+    // Extract relevant joint positions
+    const hipLeft = this.getKeypoint(pose, "left_hip");
+    const hipRight = this.getKeypoint(pose, "right_hip");
+    const kneeLeft = this.getKeypoint(pose, "left_knee");
+    const kneeRight = this.getKeypoint(pose, "right_knee");
+    const ankleLeft = this.getKeypoint(pose, "left_ankle");
+    const ankleRight = this.getKeypoint(pose, "right_ankle");
+
+    // Set thresholds
+    const variation = 80;  // Example threshold
+    const left = ((kneeLeft.y - hipLeft.y) < variation) || (kneeLeft.y <= hipLeft.y && hipLeft.y <= ankleLeft.y)
+    const right = ((kneeRight.y - hipRight.y) < variation) || (kneeRight.y <= hipRight.y && hipRight.y <= ankleRight.y)
+
+    // Check if angles indicate crouching
+    return left && right;
+  }
 
   isJumping(prevPose, currentPose) {
     if (!this.pointsExist(prevPose, ['left_ankle'])) return;
@@ -40,35 +70,28 @@ class BodyMovementsManager {
     return (currentAnkleY - prevAnkleY) > jumpThreshold;
   }
 
-  isSquatting(prevPose, currentPose) {
-    if (!this.pointsExist(prevPose, ['left_knee'])) return;
-    if (!this.pointsExist(currentPose, ['left_knee'])) return;
-    const prevKneeY = prevPose.keypoints.find(keypoint => keypoint.name === 'left_knee').y;
-    const currentKneeY = currentPose.keypoints.find(keypoint => keypoint.name === 'left_knee').y;
-    const squatThreshold = -10; // Adjust this threshold as needed
-    return (currentKneeY - prevKneeY) < squatThreshold;
-  }
-
   update() {
+    console.log("STATE:", this.state)
     if (this.detector) {
       this.detector.estimatePoses(this.webcam).then(poses => {
         const pose = poses[0];
         if (this.prevPose && pose.keypoints) {
           pose.keypoints = pose.keypoints.filter(keypoint => keypoint.score >= 0.30);
 
-          const jumpDetected = this.isJumping(this.prevPose, pose);
-          const squatDetected = this.isSquatting(this.prevPose, pose);
-          if (jumpDetected) {
-            console.log('Jump detected!');
-
-            input.setKey('space', true);
-
-          }
-          if (squatDetected) {
-            console.log('Squat detected!');
-
+          //const jumpDetected = this.isJumping(this.prevPose, pose);
+          //if (jumpDetected) {
+          //  console.log('Jump detected!');
+//
+          //  input.setKey('space', true);
+//
+          //}
+          if (this.isCrouching(pose) && this.state == Pose.NORMAL) {
+            this.state = Pose.CROUCHING;
             input.setKey('down', true);
-
+          }
+          if(!this.isCrouching(pose) && this.state == Pose.CROUCHING){
+            this.state = Pose.NORMAL;
+            input.setKey('down', false);
           }
         }
         this.prevPose = pose;
